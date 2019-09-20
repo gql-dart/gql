cfafe565f7f139fcfa78eef6f5bffff81af2c42d
Klāvs Priedītis <klavs@prieditis.lv>
2019-09-21 01:41:26 +0300

    `Link` related docs

--- gql/lib/execution.dart
+++ gql/lib/execution.dart
@@ -1,14 +1,21 @@
+/// Defines types related to Operation execution.
 @experimental
 library execution;
 
+import "package:collection/collection.dart";
 import "package:gql/ast.dart";
 import "package:meta/meta.dart";
 
+/// GraphQL Error returned if execution fails
 @immutable
 class GraphQLError {
+  /// Error message
   final String message;
-  final Iterable<ErrorLocation> locations;
-  final Iterable<dynamic /* String | int */ > path;
+  /// Locations of the nodes in document which caused the error
+  final List<ErrorLocation> locations;
+  /// Path of the error node in the query
+  final List<dynamic /* String | int */ > path;
+  /// Implementation-specific extensions to this error
   final Map<String, dynamic> extensions;
 
   const GraphQLError({
@@ -19,6 +26,7 @@ class GraphQLError {
   }) : assert(message != null);
 }
 
+/// Location of a GraphQL error in the request document
 @immutable
 class ErrorLocation {
   final int line;
@@ -31,9 +39,14 @@ class ErrorLocation {
         assert(column != null);
 }
 
+/// Execution response
 @immutable
 class Response {
-  final Iterable<GraphQLError> errors;
+  /// Error returned executing the [Request]
+  final List<GraphQLError> errors;
+  /// Data returned executing the [Request]
+  ///
+  /// Follows the shape of requested document.
   final Map<String, dynamic> data;
 
   const Response({
@@ -42,10 +55,18 @@ class Response {
   });
 }
 
+/// Container of a [document] and it's [variables]
+///
+/// Optionally defines the [operationName]
 @immutable
 class Operation {
+  /// Document containing at least one [OperationDefinitionNode]
   final DocumentNode document;
+  /// Name of the executable definition
+  ///
+  /// Must be specified if [document] contains more than one [OperationDefinitionNode]
   final String operationName;
+  /// Variables for the operation
   final Map<String, dynamic> variables;
 
   const Operation({
@@ -53,13 +74,51 @@ class Operation {
     this.operationName,
     this.variables = const <String, dynamic>{},
   }) : assert(document != null);
+
+  List<Object> _getChildren() => [
+        document,
+        operationName,
+        variables,
+      ];
+
+  @override
+  bool operator ==(Object o) =>
+      o is Operation &&
+      const DeepCollectionEquality().equals(
+        o._getChildren(),
+        _getChildren(),
+      );
+
+  @override
+  int get hashCode => const DeepCollectionEquality().hash(
+        _getChildren(),
+      );
 }
 
+/// Execution request
 @immutable
 class Request {
+  /// [Operation] to be executed
   final Operation operation;
 
   const Request({
     @required this.operation,
   }) : assert(operation != null);
+
+  List<Object> _getChildren() => [
+        operation,
+      ];
+
+  @override
+  bool operator ==(Object o) =>
+      o is Request &&
+      const DeepCollectionEquality().equals(
+        o._getChildren(),
+        _getChildren(),
+      );
+
+  @override
+  int get hashCode => const DeepCollectionEquality().hash(
+        _getChildren(),
+      );
 }
--- gql/lib/link.dart
+++ gql/lib/link.dart
@@ -1,27 +1,38 @@
+/// [DocumentNode]-based GraphQL execution interface.
 @experimental
 library link;
 
 import "package:gql/execution.dart";
 import "package:meta/meta.dart";
 
+/// Type of the `forward` function
 typedef NextLink = Stream<Response> Function(
   Request request,
 );
 
+/// [DocumentNode]-based GraphQL execution interface
 abstract class Link {
+  /// Create `Link` by chaining multiple [links]
   factory Link.from(
-    Iterable<Link> links,
+    List<Link> links,
   ) =>
       _LinkChain(links);
 
+  /// A function called when a [request] reaches this [Link]
   Stream<Response> request(
+    /// An incoming [Request]
     Request request, [
+
+    /// Function that invokes the [request] function of
+    ///   the next [Link]
+    ///
+    /// Terminating [Link]s do not call this function.
     NextLink forward,
   ]);
 }
 
 class _LinkChain implements Link {
-  final Iterable<Link> links;
+  final List<Link> links;
 
   _LinkChain(this.links);
 
@@ -30,8 +41,8 @@ class _LinkChain implements Link {
     Request request, [
     NextLink forward,
   ]) =>
-      links.toList(growable: false).reversed.fold<NextLink>(
-            forward,
-            (fw, link) => (op) => link.request(op, fw),
-          )(request);
+      links.reversed.fold<NextLink>(
+        forward,
+        (fw, link) => (op) => link.request(op, fw),
+      )(request);
 }
