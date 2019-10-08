8127601a7b7fed2a0380e991fdba6d01d4f4290f
Klāvs Priedītis <klavs@prieditis.lv>
2019-10-08 23:24:17 +0300

    implement `package:gql_transform_link`

---
+++ gql_transform_link/example/gql_transform_link_example.dart
@@ -0,0 +1,23 @@
+import "package:gql/execution.dart";
+import "package:gql_link/gql_link.dart";
+import "package:gql_transform_link/gql_transform_link.dart";
+
+void main() {
+  // ignore: unused_local_variable
+  final link = Link.from([
+    // SomeLink(),
+    TransformLink(
+      requestTransformer: (req) => req.withContextEntry(
+        RequestExtensionsThunk(
+          (req2) => req2.operation.variables,
+        ),
+      ),
+      responseTransformer: (resp) => resp.withContextEntry(
+        ResponseExtensions(
+          "an extension",
+        ),
+      ),
+    ),
+    // SomeTerminatingLink(),
+  ]);
+}
