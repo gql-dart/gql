a9087fc616cd075ba13cf509899c5ad4db923245
Sat Mandir Khalsa <smkhalsa@gmail.com>
2019-11-11 19:36:24 +0100

    add tests and reorganize test suite

---
+++ test/query_variables/variables_int_simple_test.dart
@@ -0,0 +1,67 @@
+import "package:test/test.dart";
+import 'package:gql/language.dart';
+
+import 'package:normalize/normalize.dart';
+import '../shared_data.dart';
+
+void main() {
+  group("Variables Int Simple", () {
+    final query = parseString("""
+    query TestQuery {
+      posts {
+        id
+        __typename
+        author {
+          id
+          __typename
+          name
+        }
+        title
+        comments(a: 1) {
+          id
+          __typename
+          commenter {
+            id
+            __typename
+            name
+          }
+        }
+      }
+    }
+  """);
+
+    final normalizedMap = {
+      "Query": {
+        "posts": [
+          {"\$ref": "Post:123"}
+        ]
+      },
+      "Post:123": {
+        "id": "123",
+        "__typename": "Post",
+        "author": {"\$ref": "Author:1"},
+        "title": "My awesome blog post",
+        'comments({"a":"1"})': [
+          {"\$ref": "Comment:324"}
+        ]
+      },
+      "Author:1": {"id": "1", "__typename": "Author", "name": "Paul"},
+      "Comment:324": {
+        "id": "324",
+        "__typename": "Comment",
+        "commenter": {"\$ref": "Author:2"}
+      },
+      "Author:2": {"id": "2", "__typename": "Author", "name": "Nicole"}
+    };
+
+    test("Produces correct normalized object", () {
+      expect(
+          normalize(query: query, data: sharedResponse), equals(normalizedMap));
+    });
+
+    test("Produces correct nested data object", () {
+      expect(denormalize(query: query, normalizedMap: normalizedMap),
+          equals(sharedResponse));
+    }, skip: true);
+  });
+}
