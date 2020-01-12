b6c22f3adb9d75097d60bb58d099d7e809b6ddcf
Klāvs Priedītis <klavs@prieditis.lv>
2020-01-13 00:07:57 +0200

    remove exports

--- gql_code_gen/lib/gql_code_gen.dart
+++
@@ -1,5 +0,0 @@
-/// GraphQL tools for parsing, transforming and printing GraphQL documents.
-library gql_code_gen;
-
-export "src/ast.dart";
-export "src/schema.dart";
--- gql_code_gen/lib/src/code_builders/ast.dart
+++ gql_code_gen/lib/src/code_builders/ast.dart
@@ -1,6 +1,6 @@
 import "package:code_builder/code_builder.dart";
 import "package:gql/ast.dart";
-import "package:gql_code_gen/gql_code_gen.dart";
+import "package:gql_code_gen/src/ast.dart";
 
 Library buildAstLibrary(
   DocumentNode doc,
