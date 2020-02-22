35fd9da7871455845a4e7403fd4d1b0c81b7a4d7
Klāvs Priedītis <klavs@prieditis.lv>
2020-02-22 23:00:24 +0200

    example build outputs

---
+++ gql_example_build/lib/schema.schema.gql.dart
@@ -0,0 +1,46 @@
+class Scalar {
+  const Scalar(this.value);
+
+  final String value;
+
+  @override
+  int get hashCode => value.hashCode;
+  @override
+  bool operator ==(Object o) => o is Scalar && o.value == value;
+}
+
+class Enum {
+  const Enum(this.value);
+
+  final String value;
+
+  static const Enum ON = Enum('ON');
+
+  static const Enum OFF = Enum('OFF');
+
+  @override
+  int get hashCode => value.hashCode;
+  @override
+  bool operator ==(Object o) => o is Enum && o.value == value;
+}
+
+class Input {
+  final Map<String, dynamic> input = <String, dynamic>{};
+
+  set id(String value) => input['id'] = value;
+  set bool(bool value) => input['bool'] = value;
+  set int(int value) => input['int'] = value;
+  set float(double value) => input['float'] = value;
+  set string(String value) => input['string'] = value;
+  set scalar(Scalar value) => input['scalar'] = value.value;
+  set enum$(Enum value) => input['enum'] = value.value;
+  set input(Input value) => input['input'] = value.input;
+  set idRequired(String value) => input['idRequired'] = value;
+  set boolRequired(bool value) => input['boolRequired'] = value;
+  set intRequired(int value) => input['intRequired'] = value;
+  set floatRequired(double value) => input['floatRequired'] = value;
+  set stringRequired(String value) => input['stringRequired'] = value;
+  set scalarRequired(Scalar value) => input['scalarRequired'] = value.value;
+  set enumRequired(Enum value) => input['enumRequired'] = value.value;
+  set inputRequired(Input value) => input['inputRequired'] = value.input;
+}
