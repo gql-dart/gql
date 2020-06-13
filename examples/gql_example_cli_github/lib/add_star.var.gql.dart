2b269866e2e6133057ff17db7c9a1a3dda624425
smkhalsa <smkhalsa@gmail.com>
2020-06-13 20:43:47 +0200

    update gql_example_cli_github

--- examples/gql_example_cli_github/lib/add_star.var.gql.dart
+++ examples/gql_example_cli_github/lib/add_star.var.gql.dart
@@ -1,7 +1,22 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-class AddStarVarBuilder {
-  final Map<String, dynamic> variables = <String, dynamic>{};
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+import 'package:gql_example_cli_github/serializers.gql.dart' as _i1;
 
-  set starrableId(String value) => variables['starrableId'] = value;
+part 'add_star.var.gql.g.dart';
+
+abstract class GAddStarVars
+    implements Built<GAddStarVars, GAddStarVarsBuilder> {
+  GAddStarVars._();
+
+  factory GAddStarVars([Function(GAddStarVarsBuilder b) updates]) =
+      _$GAddStarVars;
+
+  String get starrableId;
+  static Serializer<GAddStarVars> get serializer => _$gAddStarVarsSerializer;
+  Map<String, dynamic> toJson() =>
+      _i1.serializers.serializeWith(GAddStarVars.serializer, this);
+  static GAddStarVars fromJson(Map<String, dynamic> json) =>
+      _i1.serializers.deserializeWith(GAddStarVars.serializer, json);
 }
