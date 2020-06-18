3bdc17e5104c586c26a3ff682c6a6ba1342110ef
Abdullah Alsigar <abdullah.alsigar@gmail.com>
2020-06-18 15:56:39 +0300

    add `headers` to `HttpLinkResponseContext`

--- links/gql_http_link/lib/src/link.dart
+++ links/gql_http_link/lib/src/link.dart
@@ -32,13 +32,21 @@ class HttpLinkResponseContext extends ContextEntry {
   /// HTTP status code of the response
   final int statusCode;
 
+  /// HTTP response headers
+  final Map<String, String> headers;
+
   const HttpLinkResponseContext({
     @required this.statusCode,
-  }) : assert(statusCode != null);
+    @required this.headers,
+  }) : assert(
+          statusCode != null,
+          headers != null,
+        );
 
   @override
   List<Object> get fieldsForEquality => [
         statusCode,
+        headers,
       ];
 }
 
@@ -113,6 +121,7 @@ class HttpLink extends Link {
       return response.context.withEntry(
         HttpLinkResponseContext(
           statusCode: httpResponse.statusCode,
+          headers: httpResponse.headers,
         ),
       );
     } catch (e) {
