06f8f13bd6c4d0d5cbe72ec1cc30626bf1d69efe
micimize <rosenthalm93@gmail.com>
2020-07-10 22:31:10 -0500

    fix formatting

--- links/gql_http_link/test/helpers.dart
+++ links/gql_http_link/test/helpers.dart
@@ -3,12 +3,17 @@ import "dart:convert";
 
 import "package:http/http.dart" as http;
 
-http.StreamedResponse simpleResponse(String body, [int status, Map<String,String> headers = const {}]) {
+http.StreamedResponse simpleResponse(
+  String body, [
+  int status,
+  Map<String, String> headers = const {},
+]) {
   final List<int> bytes = utf8.encode(body);
   final Stream<List<int>> stream =
       Stream<List<int>>.fromIterable(<List<int>>[bytes]);
 
-  final http.StreamedResponse r = http.StreamedResponse(stream, status ?? 200, headers: headers);
+  final http.StreamedResponse r =
+      http.StreamedResponse(stream, status ?? 200, headers: headers);
 
   return r;
 }
