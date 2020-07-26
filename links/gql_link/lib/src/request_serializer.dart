b3df43e79d658f0616761b0615567b6616a2c812
micimize <rosenthalm93@gmail.com>
2020-07-26 11:14:20 -0500

    gql_link: RequestSerializer context-based inclusion

--- links/gql_link/lib/src/request_serializer.dart
+++ links/gql_link/lib/src/request_serializer.dart
@@ -1,5 +1,39 @@
 import "package:gql_exec/gql_exec.dart";
 import "package:gql/language.dart";
+import "package:meta/meta.dart";
+
+/// Context for configuring JSON [Request] serialization
+@immutable
+class RequestSerializationInclusions extends ContextEntry {
+  /// Whether to include the query document in the request. Default `true`
+  final bool query;
+
+  /// Whether to include the operation name in the request. Default `true`
+  final bool operationName;
+
+  /// Whether to include the variables in the request. Default `true`
+  final bool variables;
+
+  /// Whether to include present [RequestExtensionsThunk] results in the request. Default `true`
+  final bool extensions;
+
+  const RequestSerializationInclusions({
+    this.query = true,
+    this.operationName = true,
+    this.variables = true,
+    this.extensions = true,
+  });
+
+  static const defaults = RequestSerializationInclusions();
+
+  @override
+  List<Object> get fieldsForEquality => [
+        query,
+        operationName,
+        variables,
+        extensions,
+      ];
+}
 
 /// JSON [Request] serializer.
 class RequestSerializer {
@@ -11,11 +45,17 @@ class RequestSerializer {
   Map<String, dynamic> serializeRequest(Request request) {
     final RequestExtensionsThunk thunk = request.context.entry();
 
+    final RequestSerializationInclusions include = request.context.entry(
+      RequestSerializationInclusions.defaults,
+    );
+
     return <String, dynamic>{
-      "operationName": request.operation.operationName,
-      "variables": request.variables,
-      "query": printNode(request.operation.document),
-      if (thunk != null) "extensions": thunk.getRequestExtensions(request),
+      if (include.operationName)
+        "operationName": request.operation.operationName,
+      if (include.variables) "variables": request.variables,
+      if (include.query) "query": printNode(request.operation.document),
+      if (include.extensions && thunk != null)
+        "extensions": thunk.getRequestExtensions(request),
     };
   }
 }
