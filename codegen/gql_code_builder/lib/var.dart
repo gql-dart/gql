485c15d703b35b4bd94f172cbdceb98bfc44cf59
smkhalsa <smkhalsa@gmail.com>
2020-08-16 11:51:16 +0100

    generate variable classes for fragments

--- codegen/gql_code_builder/lib/var.dart
+++ codegen/gql_code_builder/lib/var.dart
@@ -1,21 +1,72 @@
 import "package:code_builder/code_builder.dart";
-import "package:gql_code_builder/src/operation/var.dart";
 import "package:gql_code_builder/source.dart";
+import "package:gql/ast.dart";
+import "package:gql_code_builder/src/common.dart";
+import "package:gql_code_builder/src/built_class.dart";
+
+import "package:gql_code_builder/src/frag_vars.dart";
 
 Library buildVarLibrary(
   SourceNode docSource,
   SourceNode schemaSource,
   String partUrl,
   Map<String, Reference> typeOverrides,
-) =>
-    Library(
-      (b) => b
-        ..directives.add(Directive.part(partUrl))
-        ..body.addAll(
-          buildOperationVarClasses(
-            docSource,
-            schemaSource,
-            typeOverrides,
+) {
+  final operationVarClasses = docSource.document.definitions
+      .whereType<OperationDefinitionNode>()
+      .map(
+        (op) => builtClass(
+          name: "${op.name.value}Vars",
+          getters: op.variableDefinitions.map<Method>(
+            (node) => buildGetter(
+              nameNode: node.variable.name,
+              typeNode: node.type,
+              schemaSource: schemaSource,
+              typeOverrides: typeOverrides,
+            ),
           ),
         ),
-    );
+      )
+      .toList();
+
+  Map<String, FragmentDefinitionNode> _fragmentMap(SourceNode source) => {
+        for (var def
+            in source.document.definitions.whereType<FragmentDefinitionNode>())
+          def.name.value: def,
+        for (var import in source.imports) ..._fragmentMap(import)
+      };
+
+  final fragmentVarClasses = docSource.document.definitions
+      .whereType<FragmentDefinitionNode>()
+      .map((frag) {
+        final varTypes = fragmentVarTypes(
+          fragment: frag,
+          fragmentMap: _fragmentMap(docSource),
+          schema: schemaSource.document,
+        );
+        if (varTypes.isNotEmpty) {
+          return builtClass(
+            name: "${frag.name.value}Vars",
+            getters: varTypes.entries.map<Method>(
+              (entry) => buildGetter(
+                nameNode: entry.key,
+                typeNode: entry.value,
+                schemaSource: schemaSource,
+                typeOverrides: typeOverrides,
+              ),
+            ),
+          );
+        }
+      })
+      .whereType<Class>()
+      .toList();
+
+  return Library(
+    (b) => b
+      ..directives.add(Directive.part(partUrl))
+      ..body.addAll([
+        ...operationVarClasses,
+        ...fragmentVarClasses,
+      ]),
+  );
+}
