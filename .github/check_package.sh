4b610a7b6ee696ecdebb164295d6d3b7d4481176
micimize <rosenthalm93@gmail.com>
2021-02-19 13:52:04 -0600

    add note about transience of check_package.sh

--- .github/check_package.sh
+++ .github/check_package.sh
@@ -1,5 +1,10 @@
 #/bin/bash
 
+# NOTE:
+#  This is a temporary utility script to help mitigate the need for github action
+#  troubleshooting, and will be deleted either pre-merge or when melos replaces it
+#  (https://github.com/gql-dart/gql/pull/192)
+
 set -e
 
 PACKAGE=$1
@@ -37,7 +42,7 @@ multipack --only $PACKAGE analyze --fatal-warnings --no-hints .
 # Run tests
 # mockito requires build runner now
 multipack --only $PACKAGE pub run build_runner build --delete-conflicting-outputs || true
-multipack --only $PACKAGE exec [ ! -d ./test ] && exit 0
+multipack --only $PACKAGE exec [ -d ./test ] || exit 0
 multipack --only $PACKAGE pub run test
 
 git checkout .
