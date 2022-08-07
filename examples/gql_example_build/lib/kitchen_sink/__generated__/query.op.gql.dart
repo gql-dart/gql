cfc26de5fb7bf0c31c0f6f905a00979717956658
Martin Kamleithner <martin+kamleithner@diagnosia.com>
2022-08-07 22:49:34 +0200

    chore: re-generate generated code, fix mockito dependency issue

--- examples/gql_example_build/lib/kitchen_sink/__generated__/query.op.gql.dart
+++ examples/gql_example_build/lib/kitchen_sink/__generated__/query.op.gql.dart
@@ -1,7 +1,8 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 import 'package:gql_exec/gql_exec.dart' as _i1;
-import 'package:gql_example_build/kitchen_sink/__generated__/query.ast.gql.dart' as _i2;
+import 'package:gql_example_build/kitchen_sink/__generated__/query.ast.gql.dart'
+    as _i2;
 
 const QueryOperation =
     _i1.Operation(document: _i2.document, operationName: 'QueryOperation');
