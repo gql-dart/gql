219c231ca93de76d3e02541f6c8393a03014d1a0
Raj <rajagopalvreghunath@gmail.com>
2023-07-09 22:03:20 +0530

    making the cancel token nullable

--- links/gql_dio_link/lib/src/dio_cancel_token_context_entry.dart
+++ links/gql_dio_link/lib/src/dio_cancel_token_context_entry.dart
@@ -2,7 +2,7 @@ import "package:dio/dio.dart";
 import "package:gql_exec/gql_exec.dart";
 
 class DioLinkCancelTokenContextEntry extends ContextEntry {
-  final CancelToken token;
+  final CancelToken? token;
 
   const DioLinkCancelTokenContextEntry(this.token);
 
