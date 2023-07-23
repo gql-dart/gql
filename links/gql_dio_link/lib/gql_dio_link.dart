dbb4886f19efdffd722108ec1e0eb6dbf1216d99
Raj <rajagopalvreghunath@gmail.com>
2023-07-23 20:42:46 +0530

    allow cancellation of call using dio cancellation (#408)

--- links/gql_dio_link/lib/gql_dio_link.dart
+++ links/gql_dio_link/lib/gql_dio_link.dart
@@ -2,5 +2,6 @@ library gql_dio_link;
 
 export "package:gql_exec/gql_exec.dart"
     show HttpLinkResponseContext, HttpLinkHeaders;
+export "src/dio_cancel_token_context_entry.dart";
 export "src/dio_link.dart";
 export "src/exceptions.dart";
