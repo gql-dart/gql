f78f79c10485d109f2ba8a66fbcfcadf383bf967
nhannah <ne.hannah@gmail.com>
2023-11-16 21:50:02 -0700

    Add nonDedupeTypes

--- links/gql_dedupe_link/lib/gql_dedupe_link.dart
+++ links/gql_dedupe_link/lib/gql_dedupe_link.dart
@@ -2,28 +2,37 @@
 library gql_dedupe_link;
 
 import "package:async/async.dart";
+import "package:gql/ast.dart";
 import "package:gql_exec/gql_exec.dart";
 import "package:gql_link/gql_link.dart";
 
 /// A [Link] to deduplicate [Request]s
 class DedupeLink extends Link {
+  final List<OperationType> _nonDedupeTypes;
   final Map<Request, StreamSplitter<Response>> _inFlight = {};
+
+  DedupeLink({List<OperationType> nonDedupeTypes = const []})
+      : _nonDedupeTypes = nonDedupeTypes;
 
   @override
   Stream<Response> request(
     Request request, [
     NextLink? forward,
   ]) {
-    if (_inFlight.containsKey(request)) {
+    final shouldDedupe = !_nonDedupeTypes.contains(
+      request.operation.getOperationType(),
+    );
+
+    if (shouldDedupe && _inFlight.containsKey(request)) {
       return _inFlight[request]!.split();
     }
 
     final splitter = StreamSplitter(forward!(request));
 
-    _inFlight[request] = splitter;
+    if (shouldDedupe) _inFlight[request] = splitter;
 
     final closeSplitter = () {
-      _inFlight.remove(request);
+      if (shouldDedupe) _inFlight.remove(request);
 
       splitter.close();
     };
