44c36861cf8412981c301321145a10bc4084fa6c
Marcin Hradowicz <30510627+LiLatee@users.noreply.github.com>
2024-02-23 20:53:41 +0100

    feat(gql_tristate_link): add abstentWhenNull factory

--- codegen/gql_tristate_value/test/value_test.dart
+++ codegen/gql_tristate_value/test/value_test.dart
@@ -2,33 +2,107 @@ import 'package:gql_tristate_value/gql_tristate_value.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test("requireValue on absent throws", () {
-    final absent = const AbsentValue();
+  group(
+    'requireValue',
+    () {
+      test('on AbsentValue throws', () {
+        final absent = const AbsentValue();
 
-    expect(() => absent.requireValue, throwsA(isA<StateError>()));
-  });
+        expect(() => absent.requireValue, throwsA(isA<StateError>()));
+      });
 
-  test("requireValue on present returns value", () {
-    final present = Value.present(42);
+      test('on PresentValue with valuereturns value', () {
+        final present = Value.present(42);
 
-    expect(present.requireValue, equals(42));
-  });
+        expect(present.requireValue, equals(42));
+      });
 
-  test("requireValue on null returns null", () {
-    final nullValue = Value<String>.present(null);
+      test('on PresentValue with null returns null', () {
+        final nullValue = Value<String>.present(null);
 
-    expect(nullValue.requireValue, isNull);
-  });
+        expect(nullValue.requireValue, isNull);
+      });
 
-  test("valueOrNull on absent returns null", () {
-    final absent = const AbsentValue();
+      test('on Value.ofNullable with value returns value', () {
+        final present = Value.absentWhenNull(42);
 
-    expect(absent.valueOrNull, isNull);
-  });
+        expect(present.requireValue, equals(42));
+      });
 
-  test("valueOrNull on present returns value", () {
-    final present = Value.present(42);
+      test('on Value.ofNullable with null throws', () {
+        final nullValue = Value<String>.absentWhenNull(null);
 
-    expect(present.valueOrNull, equals(42));
-  });
+        expect(() => nullValue.requireValue, throwsA(isA<StateError>()));
+      });
+    },
+  );
+
+  group(
+    'valueOrNull',
+    () {
+      test('on AbsentValue returns null', () {
+        final absent = const AbsentValue();
+
+        expect(absent.valueOrNull, isNull);
+      });
+
+      test('on PresentValue with value returns value', () {
+        final present = Value.present(42);
+
+        expect(present.valueOrNull, equals(42));
+      });
+
+      test('on Value.ofNullable with value returns value', () {
+        final present = Value.absentWhenNull(42);
+
+        expect(present.valueOrNull, equals(42));
+      });
+
+      test('on Value.ofNullable with null returns null', () {
+        final present = Value<String>.absentWhenNull(null);
+
+        expect(present.valueOrNull, isNull);
+      });
+    },
+  );
+
+  group(
+    'isPresent',
+    () {
+      test('on AbsentValue returns false', () {
+        final absent = const AbsentValue();
+
+        expect(absent.isPresent, false);
+      });
+
+      test('on PresentValue with value returns true', () {
+        final present = Value.present(42);
+
+        expect(present.isPresent, true);
+      });
+
+      test('on Value.ofNullable  with value returns true', () {
+        final present = Value.absentWhenNull(42);
+
+        expect(present.isPresent, true);
+      });
+
+      test('on Value.ofNullable with null returns false', () {
+        final present = Value<String>.absentWhenNull(null);
+
+        expect(present.isPresent, false);
+      });
+    },
+  );
+
+  group(
+    'toString',
+    () {
+      test('on PresentValue', () {
+        final present = Value.present(42);
+
+        expect(present.toString(), 'PresentValue<int>(value: 42)');
+      });
+    },
+  );
 }
